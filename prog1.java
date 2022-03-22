4a1a544dd6609a293061c1a725d3f86858a30861
Ashvani Uttam <techstudy204@gmail.com>
2022-03-22 23:37:55 +0530

    you are required to print a 'z' of size 5 using'*'

---
+++ prog1.java
@@ -0,0 +1,14 @@
+import javax.sql.rowset.spi.SyncResolver;
+
+/**
+ * prog1
+ */
+public class prog1 {
+    public static void main(String[] args) {
+        System.out.println("*****");
+        System.out.println("   * ");
+        System.out.println("  *  ");
+        System.out.println(" *   ");
+        System.out.println("*****");
+    }
+}
